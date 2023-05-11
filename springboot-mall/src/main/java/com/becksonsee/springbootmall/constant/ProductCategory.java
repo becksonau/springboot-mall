e4e317eac7c94f15efcccd3441a90b266d6ec64c
becksonau <twm0987744783@gmail.com>
2023-05-11 22:46:09 +0800

    商品功能 - Refactor category,使用 Enum 類型

---
+++ springboot-mall/src/main/java/com/becksonsee/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,11 @@
+package com.becksonsee.springbootmall.constant;
+
+public enum ProductCategory {          // 存放商品分類 - 固定值
+
+    FOOD,                               //英文大寫,多個單字用底線_連接
+    CAR,
+    BOOK,
+    E_BOOK
+
+
+}
