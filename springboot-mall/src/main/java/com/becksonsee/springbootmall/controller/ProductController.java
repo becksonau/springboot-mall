15c5fb5cedd28c1cfc8621f9060b383e66173690
becksonau <twm0987744783@gmail.com>
2023-05-14 22:09:29 +0800

    商品功能 - 實作刪除商品

--- springboot-mall/src/main/java/com/becksonsee/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/becksonsee/springbootmall/controller/ProductController.java
@@ -30,7 +30,7 @@ public class ProductController {
 
 
     @PostMapping("/products")                           // 容易忘記@Valid
-    public ResponseEntity<Product> creatProduct(@RequestBody @Valid ProductRequest productRequest) {
+    public ResponseEntity<Product> createProducte(@RequestBody @Valid ProductRequest productRequest) {
 
         Integer productId = productService.createProduct(productRequest);
 
@@ -55,6 +55,15 @@ public class ProductController {
         Product updateProduct = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.OK).body(updateProduct);
+    }
+
+
+
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();    // 204
     }
 
 
--- springboot-mall/src/main/java/com/becksonsee/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/becksonsee/springbootmall/service/ProductService.java
@@ -4,9 +4,12 @@ import com.becksonsee.springbootmall.dto.ProductRequest;
 import com.becksonsee.springbootmall.model.Product;
 
 public interface ProductService {
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
 
     void updateProduct(Integer productId, ProductRequest productRequest) ;
+
+    void deleteProductById(Integer productId);
 }
