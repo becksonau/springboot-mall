29745e4d08d8d67a56fae52df16735cb0f812cd5
becksonau <twm0987744783@gmail.com>
2023-05-17 00:00:58 +0800

    商品功能 - 實作查詢商品列表(分頁 + 總筆數)

--- springboot-mall/src/main/java/com/becksonsee/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/becksonsee/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.becksonsee.springbootmall.dto.ProductQueryParams;
 import com.becksonsee.springbootmall.dto.ProductRequest;
 import com.becksonsee.springbootmall.model.Product;
 import com.becksonsee.springbootmall.service.ProductService;
+import com.becksonsee.springbootmall.util.Page;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +26,7 @@ public class ProductController {
     private ProductService productService ;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             // 查詢條件 Filtering
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -48,11 +49,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
-
-
+        // 取得 product list
         List<Product> productList = productService.getProducts(productQueryParams) ;
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        // 取得 product 總數
+        Integer total = productService.countProduct(productQueryParams);
+
+        // 分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);                //取得商品總數
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
     }
 
 
